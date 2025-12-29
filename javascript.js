4a4ef79e3e0b4c755020bac5cd56905498f252ff
barrettstonge <238751644+barrettstonge@users.noreply.github.com>
2025-12-29 13:54:15 -0500

    learning let and const in java as well as using console for math equations

---
+++ javascript.js
@@ -0,0 +1,20 @@
+let firstName = "John";
+let lastName = "Doe";
+
+console.log(firstName);
+console.log(lastName);
+
+
+let age = 11;
+console.log(age); // outputs 11 to the console
+
+age = 54;
+ 
+console.log(age); // what will be output now?
+
+const pi = 3.14;
+
+
+console.log(pi); // What will be output?
+
+console.log((3 + 2) - 76 * (1 + 1))
